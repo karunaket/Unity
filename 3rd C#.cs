9d852b32235bd4da7c436b413a079124ad35236e
karunaket <karunaketan2077@gmail.com>
2022-12-03 22:53:29 +0530

    4th

--- 3rd C#.cs
+++ 3rd C#.cs
@@ -3,46 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
+
+    /* 
+    Insted of creating below field and property, we can create auto-property...
+    */
 {
+    // Creating auto-property...
+
+    public PlayerHealth { get; set; }
+
     private int _playerHealth = 100;  // field
 
     public int PlayerHealth  /* when there is something inside the field then it is said
-                              property...*/
+                            property...*/
     {
-        get  // The get method returns the value of the variable name...
+        get  // The get method returns the value of the variable...
         {
             return _playerHealth;
         }
 
-        set  /* The set method assigns a value to the name variable. The value keyword
+        set  /* The set method assigns a value to the variable. The value keyword
                 represents the value we assign to the property...*/
         {
             _playerHealth = value;
+        }
+
+        /* 
+        Now the value will be taken from the property not the field...
+        
+        At first it goes inside the property named PlayerHealth and grabs the set method
+        then uses to set method whatever I have wrote inside the set method inorder to assign 
+        the value to player health...
+
+        Then it grabs the get method and uses the method insted...
+
+        This is the way to change or grab the data from our field without having directly
+        refers to the field...
+        */
+
+
+        /* 
+        we can add additional codes inside the get or set methods inorder to change the
+        outcome when we use get or set method...
+If we remove the set method then we cannot assign the value of PlayerHealth 
+
+        private void Start()
+        {
+            from here
+        }
+        */
+    }
+
+    public string DisplayHealthPercentage
+        
+        /*
+        Here we have removed set method so here we only show the value we cannot assign
+        the value...
+         */
+    {
+        get
+        {
+            string health = _playerHealth.ToString() + "%";
+            /* 
+            _playerHealth is integer datatype so to convert it to string .ToString() is used
+        --> + means add another string to it...and another string is "%".
+            */
+
+            return health;
         }
     }
 
     private void Start()
     {
-        PlayerHealth = 50;
-
-        /* 
-          Now the value will be taken from the property not the field...
-        
-          At first it goes inside the property named PlayerHealth and grabs the set method
-          then uses to set method whatever I have wrote inside the set method inorder to assign 
-          the value to player health...
-
-          Then it grabs the get method and uses the method insted...
-
-          This is the way to change or grab the data from our field without having directly
-          refers to the field...
-        */
-
-
-        /* 
-          we can add additional codes inside the get or set methods inorder to change the
-          outcome when we use get or set method...
-        */
+        Debug.Log(DisplayHealthPercentage);
     }
 
 }
