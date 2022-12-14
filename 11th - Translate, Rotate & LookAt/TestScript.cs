b269b70aa5d15b309db8685728914406abbff62d
karunaket <karunaketan2077@gmail.com>
2022-12-14 23:26:05 +0530

    day12

---
+++ 11th - Translate, Rotate & LookAt/TestScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    float _moveSpeed = 7f;
+    float _rotateSpeed = 200f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(new Vector2(0f, _moveSpeed) * Time.deltaTime, Space.Self);
+
+            /* If we don't use Time.deltaTime then the gameObject sill move so fast because of higher framerate...
+              
+               If we don't use Space.Self then as a default it is going to move around the local space of the gameObject...
+               Space.Self  -->  It will help gameObject to move all around in the space...
+            */
+        }
+
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, _rotateSpeed) * Time.deltaTime, Space.Self);
+
+            // Rotate cuz now we need not to move the object...we need to rotate...
+        }
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Rotate(new Vector3(0f, 0f, -_rotateSpeed) * Time.deltaTime, Space.Self);
+        }
+    }
+}
---
+++ 11th - Translate, Rotate & LookAt/TestScript2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript2 : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    Vector2 lastRotation;
+
+    void Update()
+    {
+        Vector2 direction = target.position - transform.position;
+
+        /* 
+           This will say the gameObject at which it is attached to to rotate a certain direction based on the offset...
+        */
+
+
+        if (lastRotation != direction)
+        {
+
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+            // new Vector3(0, 1, 0) === Vector3.up/down/ left/right/ back/forward
+
+
+            // Rotates the gameObject from a certain location to a new location...
+
+            Debug.Log("test");
+
+        }
+
+        lastRotation = direction;
+    }
+}
